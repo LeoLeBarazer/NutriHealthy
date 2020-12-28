56e5e6d76650c50bbb1bf662b3fb605486988355
NeOShifT27 <72094855+NeOShifT27@users.noreply.github.com>
2020-12-28 16:52:45 +0100

    page login

--- code/client/components/connexion.vue
+++ code/client/components/connexion.vue
@@ -1,20 +1,24 @@
 <template>
     <div id ="connexion">
-        <form @submit.prevent="log">
-            <div class="div-connexion">
-                <p class="titre">Connectez-vous :</p>
-                <input class="texte-connexion" type="text" v-model="email" placeholder="Entrez votre e-mail"/>
-                <br/>
-                <input class="texte-connexion" type="text" v-model="password" placeholder="Entrez votre mot de passe"/>
-            </div>
-            <br/>
-            <div class="div-button">
-                <button class="button-connexion" type="submit">Envoyer</button>
-                <br/>
-                <p class="texte-connexion">Cliquez ici pour <a class="button-connexion" href="#/Register">creer votre compte</a></p>
-            </div>
-        </form>
+        <div class="center">
+            <h1>Login</h1>
+            <form method="post">
+                <div class="txt_field">
+                    <input type="text" required>
+                    <span></span>
+                    <label> E-mail</label>
+                </div>
+                <div class="txt_field">
+                    <input type="password1" required>
+                    <span></span>
+                    <label> Mot de passe</label>
+                </div>     
+                <input type="submit" value="Login">
+                
+            </form>
+        </div>
     </div>
+   
 </template>
 
 <script>
